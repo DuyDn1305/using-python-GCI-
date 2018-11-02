57b6c1b9b852b10a07a0de7a17f9042fe13cc22f
Duy Dn <heoconnho135135@gmail.com>
2018-11-02 14:02:43 +0700

    update some stupid mistake

--- helloWorld.py
+++ helloWorld.py
@@ -2,5 +2,6 @@ msg = "GCI is great"
 for i in range(0, 10): print(msg)
 print("Hi, what's your name ?")
 name = input()
-name = name[::-1]
 print("Hello "+name+", please to meet you!")
+name = name[::-1]
+print(name)
