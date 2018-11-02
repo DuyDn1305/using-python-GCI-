2fb04aaee58795ca860c1b7553a8b42610d461f4
Duy Dn <heoconnho135135@gmail.com>
2018-11-02 12:36:28 +0700

    upload

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,6 @@
 msg = "GCI is great"
-for i in range(1, 10): print(msg)
+for i in range(0, 10): print(msg)
 print("Hi, what's your name ?")
 name = input()
+name = name[::-1]
 print("Hello "+name+", please to meet you!")
